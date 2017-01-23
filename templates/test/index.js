a6e3fa58341e38b9d143be644f02f505709a226e
Tim Allen <tim@noblesamurai.com>
2017-01-23 13:26:48 +1100

    Better test template.

---
+++ templates/test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var chai = require('chai'),
+    expect = chai.expect;
+
+describe('my thing', function() {
+    describe('#awesome()', function() {
+        it('should work', function() {
+          throw 'unimplemented';
+        });
+    });
+});
